fa3f1ce2ac92e53380c7d5593cb63dc044fd0b4e
Mate <pejnovac.mate@gmail.com>
2023-05-07 11:42:52 +0200

    record,collection,read,linq

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
 
 namespace CsAlkfejl
 {
@@ -6,7 +10,75 @@ namespace CsAlkfejl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Generikus kollekcio
+            List<Torta> tortak = new List<Torta>();
+
+            // Record class peldanyok
+            var nagytorta = new Torta
+            {
+                Color = "white",
+                Size = "Large",
+                Sugar = 90
+            };
+            var kistorta = new Torta
+            {
+                Color = "Orange",
+                Size = "Mini",
+                Sugar = 50
+            };
+            var keskenytorta = new Torta
+            {
+                Color = "Dark",
+                Size = "Medium",
+                Sugar = 20
+            };
+            var eskuvoitorta = new Torta
+            {
+                Color = "white",
+                Size = "Large",
+                Sugar = 120
+            };
+
+            // kollekcio feltoltes
+            tortak.Add(nagytorta);
+            tortak.Add(kistorta);
+            tortak.Add(keskenytorta);
+            tortak.Add(eskuvoitorta);
+
+            // Linq
+            var results = from tortacska in tortak
+                          where tortacska.Sugar > 20
+                          orderby tortacska.Size ascending
+                          select tortacska;
+
+            foreach (var tortacska in results)
+                Console.Write(tortacska.Size + " ");
+
+            // fajlkezeles - iras
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var jsonString = JsonSerializer.Serialize(nagytorta, options);
+
+            string name = Console.ReadLine() ?? "asd.txt";
+            string path = Path.Combine(AppContext.BaseDirectory, name);
+            using (FileStream file = File.OpenWrite(path))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    try
+                    {
+                        writer.WriteLine(nagytorta);
+
+                        writer.WriteLine(jsonString);
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine("Something went wrong!");
+                    }
+                }
+            }
         }
     }
 }
---
+++ Torta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsAlkfejl
+{
+    public record Torta
+    {
+        public string Color { get; init; }
+        public string Size { get; init; }
+        public int Sugar { get; init; }
+    }
+}
